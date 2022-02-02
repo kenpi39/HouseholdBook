243403c2b4dca94943da8898c6eb38b657913352
ken39 <kenshiro393939@gmail.com>
2022-02-02 15:21:24 +0900

    ”カレンダー”画面の作成
    大まかなviewの配置

---
+++ App/app/src/main/java/com/example/mycompany/householdbook/CalendarActivity.java
@@ -0,0 +1,18 @@
+package com.example.mycompany.householdbook;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 2022/02/02.
+ */
+
+public class CalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+    }
+}

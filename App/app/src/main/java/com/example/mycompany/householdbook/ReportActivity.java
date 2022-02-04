87d952e5745aaeb130df13e52d688d3c1c717f0e
ken39 <kenshiro393939@gmail.com>
2022-02-04 19:50:29 +0900

    "レポート"画面の作成
    大まかなviewの配置
    円グラフを表示させるための準備

---
+++ App/app/src/main/java/com/example/mycompany/householdbook/ReportActivity.java
@@ -0,0 +1,17 @@
+package com.example.mycompany.householdbook;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 2022/02/04.
+ */
+
+public class ReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+    }
+}

7aea09638d91f49d0c0774aa02f6037ec957b11a
ken39 <kenshiro393939@gmail.com>
2022-02-11 17:33:36 +0900

    "支出入力"daySelectButtonを追加
    エラーが出ている

--- App/app/src/main/java/com/example/mycompany/householdbook/MainActivity.java
+++ App/app/src/main/java/com/example/mycompany/householdbook/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.mycompany.householdbook;
 
+import android.app.DatePickerDialog;
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        final Button daySelectButton = findViewById(R.id.daySelectButton);
+
+        daySelectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                Toast.makeText(MainActivity.this,
+                                        String.valueOf(year) + "/" +
+                                                String.valueOf(monthOfYear + 1) + "/" +
+                                                String.valueOf(dayOfMonth), Toast.LENGTH_SHORT).show();
+                            }
+                        },
+                        year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+
+
+
+
     }
 }

e7cd36559a7425130d6e953f5dc6712cf2941536
ken39 <kenshiro393939@gmail.com>
2022-03-04 17:23:40 +0900

    ”その他”画面遷移ボタンのコード追加

--- App/app/src/main/java/com/example/mycompany/householdbook/OthersActivity.java
+++ App/app/src/main/java/com/example/mycompany/householdbook/OthersActivity.java
@@ -1,7 +1,10 @@
 package com.example.mycompany.householdbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by user on 2022/02/10.
@@ -12,5 +15,43 @@ public class OthersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_others);
+        /*Button mainPageButton = findViewById(R.id.mainPageButton);
+
+        mainPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OthersActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button calendarPageButton = findViewById(R.id.calendarPageButton);
+
+        calendarPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OthersActivity.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button reportPageButton = findViewById(R.id.reportPageButton);
+
+        reportPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OthersActivity.this, ReportActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button othersPageButton = findViewById(R.id.othersPageButton);
+
+        othersPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });*/
     }
 }

106174842a12016c4439b885633e98c8864161d4
ken39 <kenshiro393939@gmail.com>
2022-03-04 17:20:39 +0900

    ”レポート”画面遷移ボタンのコード追加

--- App/app/src/main/java/com/example/mycompany/householdbook/ReportActivity.java
+++ App/app/src/main/java/com/example/mycompany/householdbook/ReportActivity.java
@@ -1,7 +1,10 @@
 package com.example.mycompany.householdbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by user on 2022/02/04.
@@ -13,5 +16,43 @@ public class ReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+        Button mainPageButton = findViewById(R.id.mainPageButton);
+
+        mainPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ReportActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button calendarPageButton = findViewById(R.id.calendarPageButton);
+
+        calendarPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ReportActivity.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button reportPageButton = findViewById(R.id.reportPageButton);
+
+        reportPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button othersPageButton = findViewById(R.id.othersPageButton);
+
+        othersPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ReportActivity.this, OthersActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

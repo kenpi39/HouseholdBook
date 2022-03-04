e4cd921789edff832cadc5ba58a08b71edafdf54
ken39 <kenshiro393939@gmail.com>
2022-03-04 17:21:34 +0900

    ”ヘルプ”画面遷移ボタンのコード追加

--- App/app/src/main/java/com/example/mycompany/householdbook/HelpActivity.java
+++ App/app/src/main/java/com/example/mycompany/householdbook/HelpActivity.java
@@ -1,7 +1,10 @@
 package com.example.mycompany.householdbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by user on 2022/02/10.
@@ -12,5 +15,44 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        Button mainPageButton = findViewById(R.id.mainPageButton);
+
+        mainPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HelpActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button calendarPageButton = findViewById(R.id.calendarPageButton);
+
+        calendarPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HelpActivity.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button reportPageButton = findViewById(R.id.reportPageButton);
+
+        reportPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HelpActivity.this, ReportActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button othersPageButton = findViewById(R.id.othersPageButton);
+
+        othersPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HelpActivity.this, OthersActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

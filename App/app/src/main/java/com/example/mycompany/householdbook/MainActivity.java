6d5ee042c2b7a7161b578d4023d7a06d53a86db3
ken39 <kenshiro393939@gmail.com>
2022-03-30 14:31:55 +0900

    ”支出入力”daySelectButtonの実装
    カレンダーをダイアログで表示し選択した年月日を表示するボタン

--- App/app/src/main/java/com/example/mycompany/householdbook/MainActivity.java
+++ App/app/src/main/java/com/example/mycompany/householdbook/MainActivity.java
@@ -1,44 +1,49 @@
 package com.example.mycompany.householdbook;
 
 import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.icu.util.Calendar;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Toast;
+import static android.icu.util.Calendar.getInstance;
 
 public class MainActivity extends AppCompatActivity {
+    private MainActivity main;
+    int selectedYear;
+    int selectedMonth;
+    int selectedDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        final Calendar calendar = Calendar.getInstance();
+        main = this;
+        final Calendar calendar = getInstance();
         final int year = calendar.get(Calendar.YEAR);
         final int month = calendar.get(Calendar.MONTH);
         final int day = calendar.get(Calendar.DAY_OF_MONTH);
 
         final Button daySelectButton = findViewById(R.id.daySelectButton);
-
+        daySelectButton.setText(getDateString(year, month, day));
         daySelectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final DatePickerDialog datePickerDialog = new DatePickerDialog(
-                        this,
+                final DatePickerDialog datePickerDialog = new DatePickerDialog(main,
                         new DatePickerDialog.OnDateSetListener() {
                             @Override
                             public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                                Toast.makeText(MainActivity.this,
-                                        String.valueOf(year) + "/" +
-                                                String.valueOf(monthOfYear + 1) + "/" +
-                                                String.valueOf(dayOfMonth), Toast.LENGTH_SHORT).show();
+                                daySelectButton.setText(getDateString(year, monthOfYear, dayOfMonth));
+                                selectedYear = year;
+                                selectedMonth = monthOfYear;
+                                selectedDay = dayOfMonth;
                             }
                         },
                         year, month, day);
@@ -87,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
+    private  String getDateString(int year, int month , int day){
+        return year + "年" + (month + 1) + "月" + day + "日";
     }
 }

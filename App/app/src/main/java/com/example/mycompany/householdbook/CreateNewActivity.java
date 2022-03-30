c3c4f481d41197d7079d09f5b83e44eb3140756e
ken39 <kenshiro393939@gmail.com>
2022-03-30 16:04:10 +0900

    ”新規作成”setStartButtonのOnClickListenerを実装
    ダイアログにカレンダーを表示し選択した日付をボタンに表示する

--- App/app/src/main/java/com/example/mycompany/householdbook/CreateNewActivity.java
+++ App/app/src/main/java/com/example/mycompany/householdbook/CreateNewActivity.java
@@ -1,22 +1,51 @@
 package com.example.mycompany.householdbook;
 
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.os.Bundle;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
-/**
- * Created by user on 2022/02/10.
- */
+import android.widget.DatePicker;
+import static android.icu.util.Calendar.getInstance;
 
 public class CreateNewActivity extends AppCompatActivity {
+    CreateNewActivity createNew;
+    int selectedStartYear;
+    int selectedStartMonth;
+    int selectedStartDay;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_new);
+        createNew = this;
+        final Calendar calendar = getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        Button setStartButton = findViewById(R.id.setStartButton);
+        setStartButton.setText("開始日：");
+        setStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final DatePickerDialog datePickerDialog = new DatePickerDialog(createNew,
+                    new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                            setStartButton.setText("開始日：" + getDateString(year, monthOfYear, dayOfMonth));
+                            selectedStartYear = year;
+                            selectedStartMonth = monthOfYear + 1;
+                            selectedStartDay = dayOfMonth;
+                        }
+                    },
+                    year, month, day);
+                datePickerDialog.show();
+            }
+        });
 
         Button mainPageButton = findViewById(R.id.mainPageButton);
         mainPageButton.setText("支出入力");
